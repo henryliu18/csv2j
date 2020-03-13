0a063e17b26ff5c74fe09032a2e53c4a7cd28daa
Liu <Henry.Liu@team.telstra.com>
2020-03-13 12:41:09 +1100

    adding some routes

--- controllers/sudsprod.js
+++ controllers/sudsprod.js
@@ -1,6 +1,42 @@
-// @desc    Demo
-// @route   GET /hellosuds
+// @desc    SRM feed (latest)
+// @route   GET /SRM
 // @access  Public
-exports.justademo = (req, res, next) => {
-    res.send("Hello SUDS")
+exports.srm = (req, res, next) => {
+    const csv = require('csvtojson');
+    const fs = require('fs');
+    const csvFilePath = './uploads/SRM_latest.csv';
+    if (fs.existsSync(csvFilePath)) {
+        csv()
+            .fromFile(csvFilePath)
+            .then((jsonObj) => {
+                res.send(jsonObj);
+            });
+    } else {
+        res.status(400).send({
+            status: false,
+            message: 'File does not exist',
+            data: req.params.f
+        })
+    }
+}
+
+// @desc    OVM feed1
+// @route   GET /OVM/filename1
+// @access  Public
+exports.ovm_file1 = (req, res, next) => {
+    res.send("OVM todo filename1");
+}
+
+// @desc    OVM feed2
+// @route   GET /OVM/filename2
+// @access  Public
+exports.ovm_file2 = (req, res, next) => {
+    res.send("OVM todo filename2");
+}
+
+// @desc    ITOPSA feed
+// @route   GET /ITOPSA
+// @access  Public
+exports.itopsa = (req, res, next) => {
+    res.send("ITOPSA todo");
 }
--- routes/sudsprod.js
+++ routes/sudsprod.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-const { justademo } = require('../controllers/sudsprod');
+const { 
+    srm, 
+    ovm_file1, 
+    ovm_file2, 
+    itopsa
+} = require('../controllers/sudsprod');
 
-router.route('/justademo').get(justademo);
+router.route('/SRM').get(srm);
+router.route('/OVM/file1').get(ovm_file1);
+router.route('/OVM/file2').get(ovm_file2);
+router.route('/ITOPSA').get(itopsa);
 
 module.exports = router;
