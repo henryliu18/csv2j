f6f9bc736020f103059f1f974529b66b5f87adf5
Henry Liu <henry.hhl@gmail.com>
2020-03-14 18:42:29 +1100

    +mongo/bootcamp

--- Dockerfile
+++ Dockerfile
@@ -4,9 +4,9 @@ ARG https_proxy
 RUN mkdir -p /api/routes; mkdir -p /api/controllers; mkdir -p /api/config
 WORKDIR /api
 COPY ./index.js /api/
-COPY ./routes/* /api/routes
-COPY ./controllers/* /api/controllers
-COPY ./config/* /api/config
+COPY ./routes/* /api/routes/
+COPY ./controllers/* /api/controllers/
+COPY ./config/* /api/config/
 WORKDIR /api
 RUN npm install express body-parser cors express-fileupload morgan lodash csvtojson dotenv --save
 CMD [ "node","/api/index.js" ]
--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
+const connectDB = require('./db/mongo');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
+// Body parser
+app.use(express.json());
+
+// Connect to database
+connectDB();
+
 // Route files
 const csv2j = require('./routes/csv2j');
-const test = require('./routes/test');
+const bootcamp = require('./routes/bootcamps');
 
 // Mount routers
 app.use('/api/csv2j', csv2j);
-app.use('/api/test', test);
+app.use('/api/v1/bootcamps', bootcamp);
 
 const port = process.env.PORT || 3000;
-app.listen(port, () =>
+const server = app.listen(port, () =>
   console.log(`App is listening on port ${port}.`)
 );
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server and exit process
+  server.close(() => process.exit(1));
+});
